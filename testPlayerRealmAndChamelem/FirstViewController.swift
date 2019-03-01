831a21212b438c3cc16da3cd2e1d52bb8db5f90b
rednaksu <lakhalskander@gmail.com>
2019-03-01 13:29:14 +0100

    Initial Commit

---
+++ testPlayerRealmAndChamelem/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  testPlayerRealmAndChamelem
+//
+//  Created by skander lakhal on 3/1/19.
+//  Copyright © 2019 REDRebelRiots. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
